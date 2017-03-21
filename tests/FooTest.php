503c006a3c5028bba8787523f0601c7e60b522fd
Petr Vohralík <petr@vohralik.net>
2017-03-21 15:22:54 +0100

    :octupus:

---
+++ tests/FooTest.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: petrvohralik
+ * Date: 21.03.17
+ * Time: 15:17
+ */
+class FooTest extends PHPUnit_Framework_TestCase
+{
+
+	public function testBlah()
+	{
+		$this->assertTrue(true);
+	}
+}
